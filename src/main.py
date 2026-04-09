915c16dbb40aae4bbde4aace15720cedf531fa1a
Fanatik-Karasik <tokmaktak@gmail.com>
2026-04-09 23:17:32 +0300

    Sprint 3

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
+sys.path.insert(0, os.path.join(project_root, "src"))
 
 import argparse
 
